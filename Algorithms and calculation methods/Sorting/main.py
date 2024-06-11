2c137c1d93f81e44c498c8940f3fa4492c87d875
PolinaHaieva <154555673+PolinaHaieva@users.noreply.github.com>
2024-06-11 15:35:13 +0300

    Update main.py

--- Algorithms and calculation methods/Sorting/main.py
+++ Algorithms and calculation methods/Sorting/main.py
@@ -7,9 +7,9 @@ class Lab_two:
         def first_window():
 
             self.root1 = Toplevel(self.root)
-            self.root1.title("Сортування масиву")
+            self.root1.title("Array Sorting")
             self.root.geometry("450x300")
-            Label(self.root1, font="Arial 12", text="Введіть масив").grid(
+            Label(self.root1, font="Arial 12", text="Enter the array").grid(
                 row=0, column=1, columnspan=3)
 
             entC = Entry(self.root1, font="Arial 11", width=20)
@@ -19,21 +19,21 @@ class Lab_two:
         def second_window():
 
             self.root2 = Toplevel(self.root)
-            self.root2.title("Сортування 10 масивів")
+            self.root2.title("Sorting 10 Arrays")
             self.root2.geometry("300x200")
-            Label(self.root2, font="Arial 12", text="Якщо b/z>d то y=sin(w*f)\nВ іншому випадку y=cos(w*f)").grid(
+            Label(self.root2, font="Arial 12", text="If b/z>d then y=sin(w*f)\nOtherwise y=cos(w*f)").grid(
                 row=0, column=1)
 
             self.root2.mainloop()
 
         self.root = Tk()
-        self.root.title("Лабораторна робота 2")
+        self.root.title("Lab Work 2")
         self.root.geometry("225x200")
         Label(self.root, font="Arial 13",
-              text="Гаєва Поліна\nІО-13\nСортування вибором").grid(row=0, column=1)
-        Button(self.root, font="Arial 11", text="Сортування масиву",
+              text="Selection Sort").grid(row=0, column=1)
+        Button(self.root, font="Arial 11", text="Array Sorting",
                command=first_window, width=23).grid(row=1, column=1)
-        Button(self.root, font="Arial 11", text="Сортування 10 масивів",
+        Button(self.root, font="Arial 11", text="Sorting 10 Arrays",
                command=second_window, width=23).grid(row=2, column=1)
         self.root.mainloop()
 
